0d1f60571c6ace24afb9b9ce0ef6f1019a721454
patrick <patrick@codegrafix.de>
2016-01-02 15:39:09 +0100

    SoccerBot: rename score() to loser :-). Score counts the losers :-). Add new super user command dict. separate message to types - [owner, message_type, chan, command, args] = message

--- soccerbot.py
+++ soccerbot.py
@@ -17,9 +17,13 @@ class SoccerBot(IrcBot):
         print 'Change Topic to: ' + arg
         self.send(('TOPIC %s %s\r' % (self.channel_, arg)))
 
-    def score(self, arg):
-        print '%s have lost...' % arg
-        names = arg.split(' ')
+    def set_loser(self, arg):
+        try:
+            names = arg.split(' ')
+        except ValueError:
+            print "Input not correct!"
+            return
+        print '%s are losers...' % names
         self.db.update_score(names)
         self.show_stats("")
 
@@ -30,34 +34,52 @@ class SoccerBot(IrcBot):
             print "Input not correct!"
             return
         print 'Set score of %s to %s' % (name, value)
-        self.db.set_score(name,value)
+        self.db.set_score(name, value)
         self.show_stats("")
+
+    def owner(self, args):
+        self.set_owner(args)
 
     # Command dictionary
     command_dict = {
-        ':!topic ': change_topic,
+        ':!topic': change_topic,
         ':!stats': show_stats,
-        ':!score ': score,
-        ':!set ': set_score,
+    }
+    super_user_command_dict = {
+        ':!loser': set_loser,
+        ':!set': set_score,
+        ':!owner': owner,
     }
 
+    def handle_dict_cmds(self, cmd, args, owner):
+        try:
+            if owner not in self.bot_owners:
+                print owner + " does not have the rights."
+                return
+            print 'handle command: ' + cmd + ' ' + args
+            self.super_user_command_dict[cmd](self, args)
+        except ValueError:
+            print 'Could not handle message. ValueError.'
+
     def handle_message(self, message):
-        if len(message):
-            for command in self.command_dict:
-                for line in message:
-                    if command in line:
-                        param = line.split(command)[1]
-                        print 'handle command: ' + command + param
-                        self.command_dict[command](self, param)
+        if message is None:
+            return
+        [owner, message_type, chan, command, args] = message
+
+        print message
+        if command in self.super_user_command_dict:
+            self.handle_dict_cmds(command, args, owner)
+
+        if command in self.command_dict:
+            self.command_dict[command](self, args)
 
 
-# Create and connect
 my_bot = SoccerBot("SoccerBot", "boomer.qld.au.starchat.net", 6667)
+my_bot.set_owner(':newbie|2!kvirc@Star531723.dynamic.RZ.UniBw-Muenchen.de')
 my_bot.connect('#thisisatestchan')
 my_bot.message('I am here!')
 
 while 1:
     # Loop mandatory function to keep connection with server,alive!
     # return message for further processing
-    message = my_bot.get_message
-    my_bot.handle_message(message)
+    my_bot.handle_message(my_bot.get_message)
